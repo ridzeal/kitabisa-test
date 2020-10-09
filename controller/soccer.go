95781bce922f9b28f124a25f4426bf14e9e7d185
Riadhi Aditya <ridzeal@gmail.com>
2020-10-09 22:28:02 +0700

    Add controller placeholder for team processing

---
+++ controller/soccer.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"kitabisa-test/entity"
+)
+
+// GetTeams fetching all teams in databases
+func GetTeams() (teams []entity.Team, err error) {
+	teams = []entity.Team{
+		{TeamID: 1, TeamName: "Test"},
+	}
+
+	return teams, err
+}
+
+// GetTeamDetail fetching a team detail from database
+func GetTeamDetail(teamID int) (team entity.Team, err error) {
+	team = entity.Team{
+		TeamID: 1, TeamName: "Test2",
+	}
+
+	return team, err
+}
+
+// AddTeam to database
+func AddTeam(teamName string) (teamID int, err error) {
+	teamID = 1
+	return teamID, err
+}
+
+// RemoveTeam from database
+func RemoveTeam(teamName string) (err error) {
+	return err
+}
---
+++ controller/soccer_test.go
@@ -0,0 +1,32 @@
+package controller
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSoccerGetTeamPositive(t *testing.T) {
+	teams, err := GetTeams()
+	assert.NotEqual(t, 0, len(teams), "Team list shouldn't empty")
+	assert.Equal(t, nil, err, "There should be no error")
+}
+
+func TestSoccerGetTeamDetailPositive(t *testing.T) {
+	teamID := 1
+	team, err := GetTeamDetail(teamID)
+	assert.NotEqual(t, 0, team.TeamID, "On success, team ID should be returned and not 0")
+	assert.Equal(t, nil, err, "There should be no error")
+}
+
+func TestSoccerAddTeamPositive(t *testing.T) {
+	teamName := "Real Madrid"
+	teamID, err := AddTeam(teamName)
+	assert.NotEqual(t, 0, teamID, "On success, team ID should be returned and not 0")
+	assert.Equal(t, nil, err, "There should be no error")
+}
+
+func TestSoccerRemoveTeamPositive(t *testing.T) {
+	teamID := 1
+	err := RemoveTeam(teamID)
+	assert.Equal(t, nil, err, "There should be no error")
+}
