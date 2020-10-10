64012ef698fee2a69f62f6cf28b860738b38ac36
Riadhi Aditya <ridzeal@gmail.com>
2020-10-10 15:44:02 +0700

    Remove gorm field to remove soft delete capability

--- models/player.go
+++ models/player.go
@@ -1,12 +1,7 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 // Player table structure
 type Player struct {
-	gorm.Model
 	ID int `gorm:"primary_key"`
 	Name string `gorm:"unique"`
 	Team int
